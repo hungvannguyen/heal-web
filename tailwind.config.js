16b8cf053ca9ca661d82125b7366687f4dbefd6b
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-06-14 11:18:27 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0d2b64',
+        'secondary':'#274375',
+        'tertiary':'#2f497a'
+      },
+    },
+  },
+
+  plugins: [],
+}
+

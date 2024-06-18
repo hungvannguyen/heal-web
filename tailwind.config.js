98f9c4fdb35086ff4626a1a1716d2cbe9fb937ef
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-06-18 09:50:28 +0700

    update main content,footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
       colors: {
         'primary': '#0d2b64',
         'secondary':'#274375',
-        'tertiary':'#2f497a'
+        'tertiary':'#2f497a',
+        'quaternary':'#f6f7f9',
       },
     },
   },

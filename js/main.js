b9e76d8d6fb93eb96957f9cc8231428a0e5256c3
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-06-20 14:37:07 +0700

    update home page

--- js/main.js
+++ js/main.js
@@ -8,15 +8,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const menuId = button.getAttribute('data-collapse-toggle');
             const mobileMenu = document.getElementById('mobile-menu');
+            const collapsibleElements = document.getElementsByClassName('collapsible');
             const dropdownMenu = document.getElementById(menuId);
             console.log(menuId);
             console.log(dropdownMenu);
-
-            if (currentOpenDropdown && currentOpenDropdown !== dropdownMenu && mobileMenu.classList.contains('show')) {
+            if (currentOpenDropdown && currentOpenDropdown !== dropdownMenu && mobileMenu.classList.contains('hidden') || !mobileMenu.classList.contains('lg:hidden')) {
                 currentOpenDropdown.classList.add('hidden');
                 console.log('currentOpenDropdown 1', currentOpenDropdown);
             }
 
+            if (dropdownMenu && dropdownMenu.classList.contains('collapse-target')) {
+                // In ra các lớp của dropdownMenu trước khi kiểm tra điều kiện
+                console.log('Before toggle classes:', dropdownMenu.classList);
+
+                // Toggle các lớp ngay lập tức
+                if (!dropdownMenu.classList.contains('flex') && !dropdownMenu.classList.contains('flex-wrap') && dropdownMenu.classList.contains('hidden')) {
+                    console.log('targetList true', dropdownMenu);
+                    dropdownMenu.classList.remove('hidden');
+                    dropdownMenu.classList.add('flex');
+                    dropdownMenu.classList.add('flex-wrap');
+                } else {
+                    console.log('targetList false', dropdownMenu);
+                    dropdownMenu.classList.remove('flex');
+                    dropdownMenu.classList.remove('flex-wrap');
+                    dropdownMenu.classList.add('hidden');
+                }
+
+                // In ra các lớp của dropdownMenu sau khi kiểm tra điều kiện
+                console.log('After toggle classes:', dropdownMenu.classList);
+            }
+
+            if(dropdownMenu && dropdownMenu.classList.contains('collapse-target')){
+                return;
+            }
             dropdownMenu.classList.toggle('hidden');
             currentOpenDropdown = dropdownMenu.classList.contains('hidden') ? null : dropdownMenu;
         });
